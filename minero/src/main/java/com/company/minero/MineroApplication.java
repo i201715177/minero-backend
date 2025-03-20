c8ee705d7178fa6e0a853bc9eadd03d593aaeee4
i201715177 <jimi22@hotmail.com>
2025-03-19 19:45:43 -0500

    proyecto base de control de minero

---
+++ minero/src/main/java/com/company/minero/MineroApplication.java
@@ -0,0 +1,13 @@
+package com.company.minero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MineroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MineroApplication.class, args);
+	}
+
+}
---
+++ minero/src/test/java/com/company/minero/MineroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.minero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MineroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
